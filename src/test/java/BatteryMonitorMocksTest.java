1b4b361ddd23fe2a5a79a88cd8da3f2d7ce7323c
Juan Antonio Breña Moral <bren@juanantonio.info>
2017-04-14 18:21:38 +0100

    Minor change.

--- src/test/java/BatteryMonitorMocksTest.java
+++ src/test/java/BatteryMonitorMocksTest.java
@@ -2,9 +2,11 @@ import ev3dev.utils.battery.BatteryMonitor;
 import ev3dev.utils.battery.Monitor;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,22 +14,20 @@ import java.util.List;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
+@RunWith(MockitoJUnitRunner.class)
 public class BatteryMonitorMocksTest {
-
-    @Mock
-    private Monitor monitor;
 
     @Before
     public void doBeforeEachTest() throws Exception {
         MockitoAnnotations.initMocks(this);
-        monitor = new Monitor(1000000000);
     }
 
     @Test
     public void test() throws Exception {
 
         BatteryMonitor batteryMonitor = new BatteryMonitor();
-
+        
+        Monitor monitor = Mockito.mock(Monitor.class);
         final List<Double> voltageList = new ArrayList<>();
         voltageList.add(10000000d);
         given(monitor.getVoltages()).willReturn(voltageList);
